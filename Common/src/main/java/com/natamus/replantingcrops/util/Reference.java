e0a1e4e76f40ef43413320fb34d6921f91161f18
Rick South <rick@natamus.com>
2023-12-12 00:16:27 +0100

    Replanting Crops source code for 1.20.0.

---
+++ Common/src/main/java/com/natamus/replantingcrops/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.replantingcrops.util;
+
+public class Reference {
+	public static final String MOD_ID = "replantingcrops";
+	public static final String NAME = "Replanting Crops";
+	public static final String VERSION = "5.1";
+	public static final String ACCEPTED_VERSIONS = "[1.20]";
+}
