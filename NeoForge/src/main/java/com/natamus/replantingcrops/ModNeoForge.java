7e73637567693d6b739f936390c27a0e147812c5
Rick South <rick@natamus.com>
2024-07-28 07:17:08 +0200

    (v5.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/replantingcrops/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/replantingcrops/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.replantingcrops;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.replantingcrops.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.replantingcrops.neoforge.events.NeoForgeCropEvent;
 import com.natamus.replantingcrops.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
