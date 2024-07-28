260798ee5d882b8962d481947ce867e31161b29c
Rick South <rick@natamus.com>
2024-07-28 07:17:04 +0200

    (v5.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/replantingcrops/ModForge.java
+++ Forge/src/main/java/com/natamus/replantingcrops/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.replantingcrops;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.replantingcrops.forge.config.IntegrateForgeConfig;
 import com.natamus.replantingcrops.forge.events.ForgeCropEvent;
 import com.natamus.replantingcrops.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
