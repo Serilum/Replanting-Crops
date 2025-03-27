0a922ad194b95a9c1eaa1c1fb5a16e75c938c82d
Rick South <rick@natamus.com>
2025-03-28 00:32:49 +0100

    Replanting Crops source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/replantingcrops/forge/events/ForgeCropEvent.java
@@ -0,0 +1,27 @@
+package com.natamus.replantingcrops.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.replantingcrops.events.CropEvent;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeCropEvent {
+	@SubscribeEvent
+	public static void onHarvest(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		CropEvent.onHarvest(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+	
+	@SubscribeEvent
+	public static void onCropEntity(EntityJoinLevelEvent e) {
+		if (!CropEvent.onCropEntity(e.getLevel(), e.getEntity())) {
+			e.setCanceled(true);
+		}
+	}
+}
