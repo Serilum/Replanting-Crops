54b77dcdf14078fa3ecddbbcb75eb444002206f2
Accendium <accendium.alt@gmail.com>
2025-06-07 01:12:40 -0700

    Fix: Added isAxe boolean to enable axes being held to replant cocoa

--- Common/src/main/java/com/natamus/replantingcrops/config/ConfigHandler.java
+++ Common/src/main/java/com/natamus/replantingcrops/config/ConfigHandler.java
@@ -10,11 +10,11 @@ import java.util.List;
 public class ConfigHandler extends DuskConfig {
 	public static HashMap<String, List<String>> configMetaData = new HashMap<String, List<String>>();
 
-	@Entry public static boolean mustHoldHoeForReplanting = true;
+	@Entry public static boolean mustHoldToolForReplanting = true;
 
 	public static void initConfig() {
-		configMetaData.put("mustHoldHoeForReplanting", Arrays.asList(
-			"If enabled, players must hold a hoe in their hand to automatically replant the crop."
+		configMetaData.put("mustHoldToolForReplanting", Arrays.asList(
+			"If enabled, players must hold a hoe/axe in their hand to automatically replant the crop."
 		));
 
 		DuskConfig.init(Reference.NAME, Reference.MOD_ID, ConfigHandler.class);
