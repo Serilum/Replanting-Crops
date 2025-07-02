4d565d12a6a885fda580d6ec1e6b3079edc7d2ac
Max-Leon Ernst <chiouyazo@gmail.com>
2025-07-02 13:06:51 +0200

    Only replant wheat when it is max age (and added config for thus)

--- Common/src/main/java/com/natamus/replantingcrops/config/ConfigHandler.java
+++ Common/src/main/java/com/natamus/replantingcrops/config/ConfigHandler.java
@@ -11,10 +11,14 @@ public class ConfigHandler extends DuskConfig {
 	public static HashMap<String, List<String>> configMetaData = new HashMap<String, List<String>>();
 
 	@Entry public static boolean mustHoldHoeForReplanting = true;
+	@Entry public static boolean onlyReplantFullyGrown = true;
 
 	public static void initConfig() {
 		configMetaData.put("mustHoldHoeForReplanting", Arrays.asList(
-			"If enabled, players must hold a hoe in their hand to automatically replant the crop."
+				"If enabled, players must hold a hoe in their hand to automatically replant the crop."
+		));
+		configMetaData.put("onlyReplantFullyGrown", Arrays.asList(
+				"If enabled, wheat will only be replanted if it is fully grown."
 		));
 
 		DuskConfig.init(Reference.NAME, Reference.MOD_ID, ConfigHandler.class);
