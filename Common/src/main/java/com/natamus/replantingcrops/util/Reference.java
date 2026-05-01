535329a4eec383ef19939e070899704f9c4b6859
Rick South <rick@natamus.com>
2026-05-01 23:48:08 +0200

    (v5.6) (26.1.2 NeoForge) Updated to work with NeoForge version 26.1.2.21-beta and above.

--- Common/src/main/java/com/natamus/replantingcrops/util/Reference.java
+++ Common/src/main/java/com/natamus/replantingcrops/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.replantingcrops.util;
 public class Reference {
 	public static final String MOD_ID = "replantingcrops";
 	public static final String NAME = "Replanting Crops";
-	public static final String VERSION = "5.5";
+	public static final String VERSION = "5.6";
 	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
 }
--- NeoForge/src/main/java/com/natamus/replantingcrops/neoforge/events/NeoForgeCropEvent.java
+++ NeoForge/src/main/java/com/natamus/replantingcrops/neoforge/events/NeoForgeCropEvent.java
@@ -3,13 +3,13 @@ package com.natamus.replantingcrops.neoforge.events;
 import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.replantingcrops.events.CropEvent;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
-import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 
 public class NeoForgeCropEvent {
 	@SubscribeEvent
-	public static void onHarvest(BlockEvent.BreakEvent e) {
+	public static void onHarvest(BreakBlockEvent e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
