c35900ac89d347d3133c36b555e3a4ea501bb75a
Rick South <rick@natamus.com>
2026-06-18 22:29:34 +0200

    Updated Replanting Crops to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/replantingcrops/util/Reference.java
+++ Common/src/main/java/com/natamus/replantingcrops/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "replantingcrops";
 	public static final String NAME = "Replanting Crops";
 	public static final String VERSION = "5.6";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
